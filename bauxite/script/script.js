b84a9766e5ccece1696f6bb29edf633b27aa33e7
Keith Ancajas <kancajas@gmail.com>
2018-11-28 17:18:00 +0800

    Commit

--- bauxite/script/script.js
+++ bauxite/script/script.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngAnimate']);
 
 
 // myApp.factory('JsonBinServer', function($resource){
@@ -22,7 +22,7 @@ var myApp = angular.module('myApp', []);
 
 myApp.factory('MasterLoader', function ($http) {
     var rootLink = "https://api.jsonbin.io/b/";
-    var masterLink = "5bf954aa82eab4765e812b80";//"5bdd1089baccb064c0c0e0cb";
+    var masterLink = "5bf9e8b7484e637664004b60";//"5bdd1089baccb064c0c0e0cb";
     //var colId = "5bdd0f66716f9364f8d1278b";
     var skey = "$2a$10$7AroDmsNI57xWxJ1o.1fUeb0vWv.D7.ySLUXtDQPrEcVpQ3hfRgc6";
     //var data = {};
@@ -43,60 +43,92 @@ myApp.factory('MasterLoader', function ($http) {
 
 myApp.factory('JobLoader', function ($http) {
     var rootLink = "https://api.jsonbin.io/b/";
-    var masterLink = "5bdd1089baccb064c0c0e0cb";
     var colId = "5bdd0f66716f9364f8d1278b";
-    var rowId = "$2a$10$7AroDmsNI57xWxJ1o.1fUeb0vWv.D7.ySLUXtDQPrEcVpQ3hfRgc6";
-    var data = {};
+    var skey = "$2a$10$7AroDmsNI57xWxJ1o.1fUeb0vWv.D7.ySLUXtDQPrEcVpQ3hfRgc6";
+    //var jobNo = null;
+    //var data = {};
    
     return {
-        getData: function (job) {
-            var jobNo = job;
+        getData: function (jobno) {
             var req = {
                 method: "GET",
-                url: rootLink  + jobNo + "/latest",
+                url: rootLink  + jobno + "/latest",
                 headers: {
                     "Content-Type": "application/json; charset=utf-8",
-                    "secret-key": rowId
+                    "secret-key": skey
                 }
             }
-            $http(req).then(function successCallback(response) {
-        
-                data = response;
-            }, function errorCallback(response) {
-                data = response.statusText;
-            });
-            return data;
+            return $http(req);
         }
     };
 });
 
-myApp.controller('myCtrl', function ($scope, $window, $http, $log, MasterLoader, JobLoader) {
+
+
+// myApp.factory('JobLoader', function ($http) {
+//     var rootLink = "https://api.jsonbin.io/b/";
+//     var masterLink = "5bdd1089baccb064c0c0e0cb";
+//     var colId = "5bdd0f66716f9364f8d1278b";
+//     var rowId = "$2a$10$7AroDmsNI57xWxJ1o.1fUeb0vWv.D7.ySLUXtDQPrEcVpQ3hfRgc6";
+//     var data = {};
+   
+//     return {
+//         getData: function (job) {
+//             var jobNo = job;
+//             var req = {
+//                 method: "GET",
+//                 url: rootLink  + jobNo + "/latest",
+//                 headers: {
+//                     "Content-Type": "application/json; charset=utf-8",
+//                     "secret-key": rowId
+//                 }
+//             }
+//             $http(req).then(function successCallback(response) {
+        
+//                 data = response;
+//             }, function errorCallback(response) {
+//                 data = response.statusText;
+//             });
+//             return data;
+//         }
+//     };
+// });
+
+myApp.controller('myCtrl', function ($scope, $window, $http, $filter, $log, MasterLoader, JobLoader) {
     $scope.key = null;
     $scope.record = {};
     $scope.masterArray = [];
     $scope.recordCol = [];
     $scope.jobLoaded = false;
+    $scope.hidder = new Array(20).fill(true);
 
     //initial load of masterlist
     MasterLoader.getData().then(function(response){
         $scope.masterArray = response.data;
-
-        //TODO: populate jobno dropdown
-
-
         $log.info($scope.masterArray);      
     });
 
+    //load job
     $scope.loadJob = function(jobNo){
-        $window.alert($scope.masterArray['J013763']);
-        //$scope.record = JobLoader.getData(jobNo);
-        //$log.info(JobLoader.getData(jobNo));
+        var obj = $filter('filter')($scope.masterArray.Records, {jobno: $scope.key}, true)[0];
+        $log.info(obj);
+        if (obj !== undefined){
+            JobLoader.getData(obj.key).then(function(response){
+                $scope.recordCol = response.data;
+                $log.info($scope.recordCol);
+            });           
+        }
     }
 
     $scope.newJob = function () {
 
     };
 
+
+    $scope.handleHide = function (index) {
+      
+      $scope.hidder[index] = !$scope.hidder[index];
+    }
 
     //$http.get("customers.php")
     //.then(function (response) {$scope.names = response.data.records;});
--- workshop/app.js
+++ workshop/app.js
@@ -1,5 +1,17 @@
-function sayHello(name){
-    console.log("Hello " + name);
-}
+//JSONBin API Codes
 
-sayHello("Keith");
+
+//Create Bin
+fetch(url, {
+    method: "POST",
+    body: JSON.stringify(arr),
+    headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "secret-key": rowId,
+        "collection-id": colId,
+        "name": jobNo
+    }
+}).then(function(response) {
+    if (response.ok) {
+        return response.json();
+    }
