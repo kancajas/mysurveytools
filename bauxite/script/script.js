f4eef8cf63265f6e8070006a07d87f1c2ac9ba62
Keith Ancajas <kancajas@gmail.com>
2018-12-03 16:35:43 +0800

    Commit

--- bauxite/script/script.js
+++ bauxite/script/script.js
@@ -64,6 +64,38 @@ myApp.factory('JobLoader', function ($http) {
 });
 
 
+myApp.directive('contenteditable', ['$sce', function($sce) {
+    return {
+      restrict: 'A', // only activate on element attribute
+      require: '?ngModel', // get a hold of NgModelController
+      link: function(scope, element, attrs, ngModel) {
+        if (!ngModel) return; // do nothing if no ng-model
+  
+        // Specify how UI should be updated
+        ngModel.$render = function() {
+          element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+        };
+  
+        // Listen for change events to enable binding
+        element.on('blur keyup change', function() {
+          scope.$evalAsync(read);
+        });
+        read(); // initialize
+  
+        // Write data to the model
+        function read() {
+          var html = element.html();
+          // When we clear the content editable the browser leaves a <br> behind
+          // If strip-br attribute is provided then we strip this out
+          if ( attrs.stripBr && html == '<br>' ) {
+            html = '';
+          }
+          ngModel.$setViewValue(html);
+        }
+      }
+    };
+  }]);
+
 
 // myApp.factory('JobLoader', function ($http) {
 //     var rootLink = "https://api.jsonbin.io/b/";
@@ -101,33 +133,42 @@ myApp.controller('myCtrl', function ($scope, $window, $http, $filter, $log, Mast
     $scope.recordCol = [];
     $scope.jobLoaded = false;
     $scope.hidder = new Array(20).fill(true);
+    $scope.editMode = true;
 
     //initial load of masterlist
     MasterLoader.getData().then(function(response){
         $scope.masterArray = response.data;
-        $log.info($scope.masterArray);      
+       // $log.info($scope.masterArray);      
     });
 
-    //load job
+    //load job on button press
     $scope.loadJob = function(jobNo){
         var obj = $filter('filter')($scope.masterArray.Records, {jobno: $scope.key}, true)[0];
         $log.info(obj);
         if (obj !== undefined){
             JobLoader.getData(obj.key).then(function(response){
-                $scope.recordCol = response.data;
-                $log.info($scope.recordCol);
+                
+                $scope.recordCol = response.data;     
+                $scope.recordCol["Datetime"] = $scope.convertDate($scope.recordCol["Datetime"]);
+                $log.info($scope.recordCol);           
+                $scope.jobLoaded = true;
             });           
         }
-    }
+    };
 
     $scope.newJob = function () {
 
     };
 
+    $scope.convertDate = function(dateString){
+        var s = dateString;
+        var b = s.split(/\D+/);
+        return new Date(b[0], --b[1], b[2], b[3], b[4], b[5]||0, b[6]||0);
+    };
 
-    $scope.handleHide = function (index) {
-      
-      $scope.hidder[index] = !$scope.hidder[index];
+
+    $scope.handleHide = function (index) {     
+        $scope.hidder[index] = !$scope.hidder[index];
     }
 
     //$http.get("customers.php")
