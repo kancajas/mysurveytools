8b8c4490f00c63b9c86f50d447a0fd5f512126ca
Keith Ancajas <kancajas@gmail.com>
2018-12-04 23:42:52 +0800

    Commit

--- bauxite/script/script.js
+++ bauxite/script/script.js
@@ -41,27 +41,69 @@ myApp.factory('MasterLoader', function ($http) {
     };
 });
 
-myApp.factory('JobLoader', function ($http) {
+myApp.factory('JobHandler', function ($http, $window,$log) {
     var rootLink = "https://api.jsonbin.io/b/";
     var colId = "5bdd0f66716f9364f8d1278b";
-    var skey = "$2a$10$7AroDmsNI57xWxJ1o.1fUeb0vWv.D7.ySLUXtDQPrEcVpQ3hfRgc6";
+    var sKey = "$2a$10$7AroDmsNI57xWxJ1o.1fUeb0vWv.D7.ySLUXtDQPrEcVpQ3hfRgc6";
     //var jobNo = null;
     //var data = {};
    
     return {
-        getData: function (jobno) {
+        pullData: function (jobKey) {
             var req = {
                 method: "GET",
-                url: rootLink  + jobno + "/latest",
+                url: rootLink + jobKey + "/latest",
                 headers: {
                     "Content-Type": "application/json; charset=utf-8",
-                    "secret-key": skey
+                    "secret-key": sKey
                 }
             }
-            return $http(req);
+            return $http(req).then(successCallback, errorCallback);
+        },
+        pushData: function (jobKey,recordCol) {
+            var jobLink = rootLink + jobKey;
+            var req = {
+                method: "PUT",
+                data: angular.toJson(recordCol),
+                url: jobLink,
+                headers: {
+                    "Content-Type": "application/json",
+                    "secret-key": sKey
+                }
+            }
+            return $http(req).then(successCallback, errorCallback);
+        },
+        createData: function (recordCol) {
+            var jobLink = rootLink + jobKey;
+            var req = {
+                method: "PUT",
+                data: angular.toJson(recordCol),
+                url: jobLink,
+                headers: {
+                    "Content-Type": "application/json",
+                    "secret-key": sKey
+                }
+            }
+            return $http(req).then(successCallback, errorCallback);
         }
+
+
     };
+
+    function successCallback(response) {
+        return response;
+    }
+
+    function errorCallback(response) {
+        $window.alert("Something went wrong! It's FUBAR..");
+        return response;
+    }
+
+ 
+
 });
+
+
 
 
 myApp.directive('contenteditable', ['$sce', function($sce) {
@@ -97,7 +139,7 @@ myApp.directive('contenteditable', ['$sce', function($sce) {
   }]);
 
 
-// myApp.factory('JobLoader', function ($http) {
+// myApp.factory('JobHandler', function ($http) {
 //     var rootLink = "https://api.jsonbin.io/b/";
 //     var masterLink = "5bdd1089baccb064c0c0e0cb";
 //     var colId = "5bdd0f66716f9364f8d1278b";
@@ -126,7 +168,7 @@ myApp.directive('contenteditable', ['$sce', function($sce) {
 //     };
 // });
 
-myApp.controller('myCtrl', function ($scope, $window, $http, $filter, $log, MasterLoader, JobLoader) {
+myApp.controller('myCtrl', function ($scope, $window, $http, $filter, $log, MasterLoader, JobHandler) {
     $scope.key = null;
     $scope.record = {};
     $scope.masterArray = [];
@@ -135,6 +177,12 @@ myApp.controller('myCtrl', function ($scope, $window, $http, $filter, $log, Mast
     $scope.hidder = new Array(20).fill(true);
     $scope.editMode = true;
 
+    $scope.jobItem = function() {
+        this.Booking = "SHIT";
+        this.Records = [{Booking:"ERERER"}];
+        
+     };
+     
     //initial load of masterlist
     MasterLoader.getData().then(function(response){
         $scope.masterArray = response.data;
@@ -144,21 +192,112 @@ myApp.controller('myCtrl', function ($scope, $window, $http, $filter, $log, Mast
     //load job on button press
     $scope.loadJob = function(jobNo){
         var obj = $filter('filter')($scope.masterArray.Records, {jobno: $scope.key}, true)[0];
-        $log.info(obj);
+        $log.info("Loading: "+ obj.jobno + " " + obj.key);
         if (obj !== undefined){
-            JobLoader.getData(obj.key).then(function(response){
-                
-                $scope.recordCol = response.data;     
-                $scope.recordCol["Datetime"] = $scope.convertDate($scope.recordCol["Datetime"]);
-                $log.info($scope.recordCol);           
-                $scope.jobLoaded = true;
+            JobHandler.pullData(obj.key).then(function(response){ 
+                if (response.data.success === false){
+                    $window.alert("Opps: " + response.message);
+                }else{
+                    $scope.recordCol = response.data;     
+                    $scope.recordCol["Datetime"] = $scope.convertDate($scope.recordCol["Datetime"]);
+                    $scope.jobLoaded = true;
+                    $log.info("Success: " + $scope.recordCol); 
+                }
+            });           
+        }
+    };
+
+    $scope.saveJob = function(){
+        var obj = $filter('filter')($scope.masterArray.Records, {jobno: $scope.key}, true)[0];
+        $log.info("Saving: "+ obj.jobno + " " + obj.key);
+        if (obj !== undefined){
+            JobHandler.pushData(obj.key,$scope.recordCol).then(function(response){ 
+                if (!response.data.success){
+                    $window.alert("Opps: " + response.message);
+                }else{
+                    $log.info("Success: " + $scope.recordCol); 
+                }
             });           
         }
     };
 
     $scope.newJob = function () {
-
+        //check validity of jobno
+        //check if existing in masterArray
+        //if existing, load instead
+        var obj = $filter('filter')($scope.masterArray.Records, {jobno: $scope.key}, true)[0];
+        if (obj === undefined){
+            $scope.recordCol.Records = [];
+            $scope.addItem();
+            $scope.jobLoaded = true;
+        }
     };
+
+    $scope.closeJob = function () {
+        $scope.recordCol = angular.copy({},$scope.master);
+        var today = new Date();
+        today.setSeconds(0,0);
+        $scope.recordCol["Datetime"] = today;
+        $scope.key = null;
+        $scope.jobLoaded = false;
+    };
+
+    $scope.addItem = function() {
+
+        // $scope.recordCol = {
+        //     "Description": "J013763",
+        //     "Paratwo": "",
+        //     "Paraone": "Archived Files",
+        //     "Record": 18,
+        //     "Gallery": "",
+        //     "Vessel": "PHOENIX LEADER",
+        //     "Voynum": "13",
+        //     "Surveyloc": "Berth 12, North Quay, Fremantle",
+        //     "Datetime": "2018-11-03T12:05:00.000Z",
+        //     "Jobno": "J013763",
+        //     "Records": [
+        //       {
+        //         "Completed": false,
+        //         "Comments": "Test This Test Test Test!!! Yep! Yep!",
+        //         "Exceptions": "Nil",
+        //         "Weight": "33.910",
+        //         "Cubic": "154.72",
+        //         "Height": "3.9",
+        //         "Width": "3.42",
+        //         "Length": "11.6",
+        //         "Condition": "Used",
+        //         "Unit": "Tracked excavator cat 336D (to be re-measured on board)",
+        //         "Booking": "E002103430 (1) x"
+        //       }
+        //     ]
+        //   };
+
+
+      
+        $scope.recordCol.Records.push({});
+        $log.info($scope.recordCol);
+    }
+
+
+    $scope.getToday = function () {
+        var d = new Date();
+        var hr;
+        var min;
+        var today = d.toISOString().substr(0, 10);
+        if (d.getHours() < 10) {
+            hr = "0" + d.getHours();
+        } else {
+            hr = d.getHours();
+        }
+        if (d.getMinutes() < 10) {
+            min = "0" + d.getMinutes();
+        } else {
+            min = d.getMinutes();
+        }
+        return today + "T" + hr + ":" + min;
+    };
+    
+
 
     $scope.convertDate = function(dateString){
         var s = dateString;
